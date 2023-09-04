f8896a6f3f7bba9f33fee64e9e4a1be0902e15e3
Əziz Rəhimov <72435144+ezizrehimov@users.noreply.github.com>
2023-09-04 13:28:36 +0400

    completed

---
+++ 2SepTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2SepTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("value: " + value);
+
+            if (value % 3 == 0)
+            {
+                if (value / 3 % 3 == 0)
+                {
+                    Console.WriteLine("duzdu");
+                }
+                else
+                {
+                    Console.WriteLine("qaliqsiz bolundu, amma quvveti deyil");
+                }
+            }
+            else
+            {
+                Console.WriteLine("qaliqsiz bolunmedi");
+            }
+        }
+    }
+
+}
